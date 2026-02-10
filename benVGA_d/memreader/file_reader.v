3a5a42245fb09daa7df691a1589f884c267289c1
smorebobben <smorebobben@gmail.com>
2026-02-09 18:15:46 -0800

    working on reading from file

--- benVGA_d/memreader/file_reader.v
+++ benVGA_d/memreader/file_reader.v
@@ -4,6 +4,25 @@ module file_reader (
     input [18:0] addr_i,
     output [7:0] pixel_o
 );
-    assign pixel_o = 8'hfa;
+    assign pixel_o = addr_i[17:10];
+
+    reg[7:0] str;
+    integer fd;
+    integer chars;
+
+    initial begin
+        fd = $fopen("frame_0.bmp", "rb");
+
+        // Keep reading lines until EOF is found
+        while (! $feof(fd)) begin
+
+  	    // Get current line into the variable 'str'
+            chars = $fgets(str, fd);
+
+            // Display contents of the variable
+            $display("%0s", str);
+        end
+        $fclose(fd);
+    end
 
 endmodule
