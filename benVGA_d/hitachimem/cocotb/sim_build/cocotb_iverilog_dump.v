8c6ccb3614d209ee1354a6f2e33d01392fb50062
smorebobben <smorebobben@gmail.com>
2026-03-02 13:15:58 -0800

    changing VGA directory to just VGA instead of benVGA_d

--- benVGA_d/hitachimem/cocotb/sim_build/cocotb_iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module cocotb_iverilog_dump();
-initial begin
-    $dumpfile("sim_build/hmem_access.fst");
-    $dumpvars(0, hmem_access);
-end
-endmodule
--- benVGA_d/memreader/cocotbtest/sim_build/cocotb_iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module cocotb_iverilog_dump();
-initial begin
-    $dumpfile("sim_build/mem2vga.fst");
-    $dumpvars(0, mem2vga);
-end
-endmodule
