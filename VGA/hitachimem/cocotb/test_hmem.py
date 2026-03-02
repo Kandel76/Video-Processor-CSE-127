dfad125e802cc159e926265817eef0a8a477d0cb
smorebobben <smorebobben@gmail.com>
2026-03-02 15:51:23 -0800

    simulating 2 memory chips to test chip select

--- VGA/hitachimem/cocotb/test_hmem.py
+++ VGA/hitachimem/cocotb/test_hmem.py
@@ -2,6 +2,7 @@
 # Uses helper functions to simulate the external memory
 import cocotb
 from cocotb.triggers import FallingEdge, Timer, RisingEdge, First
+import logging
 
 # helpers ===========================================================
 async def generate_clock(dut, NUMCYCLES=200):
@@ -49,8 +50,14 @@ async def external_mem1(dut):    # simulated external memory 1
     # writing
     while (1):
         action = await First(FallingEdge(dut.nWE), FallingEdge(dut.nOE)) # wait for a read or write to start
+        await Timer(1, unit="ps") #looking for the values which are held for 3 cycles
+        # there seems to be a race condition somewhere, but the values should be held for 3 cycles
+        # so idk man Im just gonna leave this wait here
 
         if (dut.nCS1_o.value == 0): #if chip one is selected
+
+            if (dut.nCS2_o.value == 0):
+                print(" ! ERROR: MEM1: Both chip select values are high")
 
             if (action == FallingEdge(dut.nWE)): #WRITE
                 await Timer(85, unit="ns")
@@ -62,16 +69,50 @@ async def external_mem1(dut):    # simulated external memory 1
                     await Timer(85, unit="ns")
                     dut.data_i.value = sim_memory[int(dut.addr_o.value)]
                 else:
-                    print(" ! ERROR: Attempted to access nonexistent memory address", int(dut.addr_o.value))
+                    print(" ! ERROR: MEM1: Attempted to access nonexistent memory address", int(dut.addr_o.value))
+
+async def external_mem2(dut):    # simulated external memory 2
+    #dictionary to simulate specified addresses
+    # look for low nCS2
+    # TODO note: ***THIS DOES NOT PRECISELY CHECK THE TIMINGS***
+    sim_memory2 = dict()
+
+    # writing
+    while (1):
+        action = await First(FallingEdge(dut.nWE), FallingEdge(dut.nOE)) # wait for a read or write to start
+        await Timer(1, unit="ps") #looking for the values which are held for 3 cycles
+        # there seems to be a race condition somewhere, but the values should be held for 3 cycles
+        # so idk man Im just gonna leave this wait here
+
+        if (dut.nCS2_o.value == 0): #if chip two is selected
+
+            if (dut.nCS1_o.value == 0):
+                print(" ! ERROR: MEM2: Both chip select values are high")
+
+            if (action == FallingEdge(dut.nWE)): #WRITE
+                await Timer(85, unit="ns")
+                sim_memory2[int(dut.addr_o.value)] = dut.data_o.value
+            elif (action == FallingEdge(dut.nOE)): # READ
+                dut.data_i.value = 0xbd #bad data value. If you see this in output, there's an error in timing
+                if (int(dut.addr_o.value) in sim_memory2.keys()):
+                    await Timer(85, unit="ns")
+                    dut.data_i.value = sim_memory2[int(dut.addr_o.value)]
+                else:
+                    print(" ! ERROR: MEM2: Attempted to access nonexistent memory address", int(dut.addr_o.value))
+
 
 
 # tests =============================================================
 @cocotb.test()
 async def small_test(dut):
+    # cocotb setup stuff
+    logger = logging.getLogger("memtest")
+
     # cocotb.start_soon(generate_clock(dut))
     cocotb.start_soon(generate_clk_and_reset(dut, 80000))
 
     cocotb.start_soon(external_mem1(dut))
+    cocotb.start_soon(external_mem2(dut))
 
     await FallingEdge(dut.reset) # extra wait to test reset
     #initalize values
@@ -99,7 +140,7 @@ async def small_test(dut):
         await RisingEdge(dut.rvalid_o)
         await RisingEdge(dut.clk)
         if (dut.rdata_o.value != i):
-            print(" ! ERROR: expected value: ", i, " got value: ", dut.rdata_o.value)
+            print(" ! ERROR: expected value: ", i, " got value: ", int(dut.rdata_o.value))
 
     # New test ======================================================
     for i in range(6):
@@ -113,17 +154,41 @@ async def small_test(dut):
         await FallingEdge(dut.clk)
 
     for i in range(1, 32):
-        dut.waddr_i.value = int(i + 0x0100)
+        dut.waddr_i.value = int(i + 0x7000)
         dut.wdata_i.value = i
         for _ in range(6):
             await FallingEdge(dut.clk)
 
     for i in range(1, 32):
-        dut.raddr_i.value = int(i + 0x0100)
+        dut.raddr_i.value = int(i + 0x7000)
         await RisingEdge(dut.rvalid_o)
         await RisingEdge(dut.clk)
         if (dut.rdata_o.value != i):
-            print(" ! ERROR: expected value: ", i, " got value: ", dut.rdata_o.value)
+            print(" ! ERROR: expected value: ", i, " got value: ", int(dut.rdata_o.value))
+    
+    # New test ======================================================
+    for i in range(6):
+        await FallingEdge(dut.clk)
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)
+    print(" === Bigger Address Test === ")
+    await FallingEdge(dut.clk)
+    dut.reset.value = 0
+    for i in range(6):
+        await FallingEdge(dut.clk)
+
+    for i in range(1, 32):
+        dut.waddr_i.value = int(i + 0x8000)
+        dut.wdata_i.value = i
+        for _ in range(6):
+            await FallingEdge(dut.clk)
+
+    for i in range(1, 32):
+        dut.raddr_i.value = int(i + 0x8000)
+        await RisingEdge(dut.rvalid_o)
+        await RisingEdge(dut.clk)
+        if (dut.rdata_o.value != i):
+            print(" ! ERROR: expected value: ", hex(i), " got value: ", hex(int(dut.rdata_o.value)))
     
     
     
