528fda675e9f961e7c6875a95bef2f1a394f0c3e
aaroy13 <aarroy13@ucsc.edu>
2026-04-09 14:50:09 -0700

    convert comparator to gf180 cells

--- comparator/comparator.sv
+++ comparator/comparator.sv
@@ -5,36 +5,61 @@ module comparator (
     output [0:0] q_o,
     output [0:0] q_invo
 );
-//inverted signal to drive comparator NOR3's
-logic [0:0] clk_inv;
-assign clk_inv = ~clk_i;
+    //inverted signal to drive comparator NOR3's
+    wire clk_inv;
 
-//NOR3A
-logic [0:0] norA_o; 
-assign norA_o = (~v_inp) & (~clk_inv) & (~norB_o);
+    //internal latch nodes
+    wire norA_o;
+    wire norB_o;
+    wire nandA_o;
+    wire nandB_o;
 
-//NOR3B
-logic [0:0] norB_o; 
-assign norB_o = (~v_inm) & (~clk_inv) & (~norA_o);
+    //invert clk
+    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__inv_1 u_inv_clk (
+        .I(clk_i[0]),
+        .ZN(clk_inv)
+    );
+    
+    //NOR3A
+    //assign norA_o = (~v_inp) | (~clk_inv) | (~norB_o);
+    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__nor3_1 u_norA (
+        .A1(v_inp[0]),
+        .A2(clk_inv),
+        .A3(norB_o),
+        .ZN(norA_o)
+    );
+    
+    //NOR3B
+    //assign norB_o = (~v_inm) | (~clk_inv) | (~norA_o);
+    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__nor3_1 u_norB (
+        .A1(v_inm[0]),
+        .A2(clk_inv),
+        .A3(norA_o),
+        .ZN(norB_o)
+    );
 
-//NAND3A
-logic [0:0] nandA_o; 
-assign nandA_o = ~(v_inm & clk_i & nandB_o);
-//Invert NAND3A
-logic [0:0] nandA_inv; 
-assign nandA_inv = ~nandA_o; 
+    //NAND3A
+    //assign nandA_o = ~(v_inm & clk_i & nandB_o);
+    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__nand3_1 u_nandA (
+        .A1(v_inm[0]),
+        .A2(clk_i[0]),
+        .A3(nandB_o),
+        .ZN(nandA_o)
+    );
 
-//NAND3B
-logic [0:0] nandB_o; 
-assign nandB_o = ~(v_inp & clk_i & nandA_o);
-//Invert NAND3B
-logic [0:0] nandB_inv; 
-assign nandB_inv = ~nandB_o; 
+    //NAND3B
+    //assign nandB_o = ~(v_inp & clk_i & nandA_o);
+    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__nand3_1 u_nandB (
+        .A1(v_inp[0]),
+        .A2(clk_i[0]),
+        .A3(nandA_o),
+        .ZN(nandB_o)
+    );
 
-//Route these into 2 NOR3 gates 
-//q_invo
-assign q_invo = ~(nanaA_inv) & ~(norA_o) & ~(q_o);
-//q_o
-assign q_o = ~(nandB_inv) & ~(norB_o) & ~(q_invo);
+    //Route these into 2 NOR3 gates 
+    //q_invo
+    assign q_invo[0] = norB_o;   //~(nanaA_inv) & ~(norA_o) & ~(q_o);
+    //q_o
+    assign q_o[0] = norA_o;   //~(nandB_inv) & ~(norB_o) & ~(q_invo);
 
 endmodule
