fcfca518287a0613cc7847fa1859bd9cd1aee574
Tlaloc Ruiz <tlaruiz@ucsc.edu>
2026-06-09 15:51:31 -0700

    updated comp to 3.3

--- src/ADC/comparator/comparator.sv
+++ src/ADC/comparator/comparator.sv
@@ -17,74 +17,74 @@ module comparator (
     wire nandB_inv;
 
     //invert clk
-    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__inv_1 u_inv_clk (
-        .I(clk_i[0]),
-        .ZN(clk_inv)
+    (* dont_touch = "true" *) gf180mcu_as_sc_mcu7t3v3__inv_2 u_inv_clk (
+        .A(clk_i[0]),
+        .Y(clk_inv)
     );
     
     //NOR3A
     //assign norA_o = ~(v_inp | clk_inv | norB_o);
-    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__nor3_1 u_norA (
-        .A1(v_inp[0]),
-        .A2(clk_inv),
-        .A3(norB_o),
-        .ZN(norA_o)
+    (* dont_touch = "true" *) gf180mcu_as_sc_mcu7t3v3__nor3_2 u_norA (
+        .A(v_inp[0]),
+        .B(clk_inv),
+        .C(norB_o),
+        .Y(norA_o)
     );
     
     //NOR3B
     //assign norB_o = ~(v_inm | clk_inv | norA_o);
-    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__nor3_1 u_norB (
-        .A1(v_inm[0]),
-        .A2(clk_inv),
-        .A3(norA_o),
-        .ZN(norB_o)
+    (* dont_touch = "true" *) gf180mcu_as_sc_mcu7t3v3__nor3_2 u_norB (
+        .A(v_inm[0]),
+        .B(clk_inv),
+        .C(norA_o),
+        .Y(norB_o)
     );
 
     //NAND3A
     //assign nandA_o = ~(v_inm & clk_i & nandB_o);
-    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__nand3_1 u_nandA (
-        .A1(v_inm[0]),
-        .A2(clk_i[0]),
-        .A3(nandB_o),
-        .ZN(nandA_o)
+    (* dont_touch = "true" *) gf180mcu_as_sc_mcu7t3v3__nand3_2 u_nandA (
+        .A(v_inm[0]),
+        .B(clk_i[0]),
+        .C(nandB_o),
+        .Y(nandA_o)
     );
 
     //NAND3A_INV
-    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__inv_1 u_inv_nandA (
-        .I(nandA_o),
-        .ZN(nandA_inv)
+    (* dont_touch = "true" *) gf180mcu_as_sc_mcu7t3v3__inv_2 u_inv_nandA (
+        .A(nandA_o),
+        .Y(nandA_inv)
     );
 
     //NAND3B
     //assign nandB_o = ~(v_inp & clk_i & nandA_o);
-    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__nand3_1 u_nandB (
-        .A1(v_inp[0]),
-        .A2(clk_i[0]),
-        .A3(nandA_o),
-        .ZN(nandB_o)
+    (* dont_touch = "true" *) gf180mcu_as_sc_mcu7t3v3__nand3_2 u_nandB (
+        .A(v_inp[0]),
+        .B(clk_i[0]),
+        .C(nandA_o),
+        .Y(nandB_o)
     );
 
     //NAND3B_INV
-    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__inv_1 u_inv_nandB (
-        .I(nandB_o),
-        .ZN(nandB_inv)
+    (* dont_touch = "true" *) gf180mcu_as_sc_mcu7t3v3__inv_2 u_inv_nandB (
+        .A(nandB_o),
+        .Y(nandB_inv)
     );
 
     //Route these into 2 NOR3 gates 
     //q_invo = ~(nandA_inv | norA_o | q_o);
-    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__nor3_1 u_q_inv (
-        .A1(nandA_inv),
-        .A2(norA_o),
-        .A3(q_o[0]),
-        .ZN(q_invo[0])
+    (* dont_touch = "true" *) gf180mcu_as_sc_mcu7t3v3__nor3_2 u_q_inv (
+        .A(nandA_inv),
+        .B(norA_o),
+        .C(q_o[0]),
+        .Y(q_invo[0])
     );
     
     //q_o = ~(nandB_inv | norB_o | q_invo);
-    (* dont_touch = "true" *) gf180mcu_fd_sc_mcu9t5v0__nor3_1 u_q_o (
-        .A1(nandB_inv),
-        .A2(norB_o),
-        .A3(q_invo[0]),
-        .ZN(q_o[0])
+    (* dont_touch = "true" *) gf180mcu_as_sc_mcu7t3v3__nor3_2 u_q_o (
+        .A(nandB_inv),
+        .B(norB_o),
+        .C(q_invo[0]),
+        .Y(q_o[0])
     );
 
 endmodule
